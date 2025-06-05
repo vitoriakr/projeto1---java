9fdd1774c056ea0df7db36f53138352951e9136a
vitoriakr <vkereski@gmail.com>
2025-06-05 20:57:54 -0300

    novas atividades de java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
  public class Main {
     public static void main(String[] args) {
-        System.out.println("alõ mundoooooooooo!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento" + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.2;
+        double media = (6.0+10.0+8.0)/3;/*media feita por 3 notas aleatorias*/
+        System.out.println(media);
+        String sinopse = """
+                Filme de aventura comgalã dos anos 80
+                Muito bom!
+                """;
+        System.out.println(sinopse);
+        //casting inplicito double classificacao = media/2;
+        int classificacao = (int) (media/2);/*casting explicito*/
+        System.out.println(classificacao);
     }
 }
--- src/Perfil.java
+++ src/Perfil.java
@@ -2,5 +2,6 @@ public class Perfil {
     public static void main(String[] args){
         String nome = "Vitória";
         System.out.println("olá, "+nome+"!");
+        System.out.println("Tudo bem, "+nome+"?");
     }
 }
